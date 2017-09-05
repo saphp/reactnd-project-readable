09f9fcdcc6889a0f57e2df6ff52d5a1be7e2f7b9
Saleh Almatrafi <smatrafi@tamkeentech.sa>
2017-09-06 01:39:29 +0300

    setup store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,14 @@
-// Reducers
+import { combineReducers } from 'redux'
+
+function posts (state = {}, action) {
+	return state
+}
+
+function comments (state = {}, action) {
+	return state
+}
+
+export default combineReducers({
+	posts,
+	comments,
+})
